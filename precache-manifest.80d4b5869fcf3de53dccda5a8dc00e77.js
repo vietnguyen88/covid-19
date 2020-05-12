8177565441cdfe89b189c59fb53e5922f600700a
vietnguyen <vietnguyen.nqv@gmail.com>
2020-05-12 22:51:26 +1000

    Updates

---
+++ precache-manifest.80d4b5869fcf3de53dccda5a8dc00e77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02eb60a4076878fd01c8f6a521de58cc",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "7e00445f6d038130d8e2",
+    "url": "/covid-19/static/css/main.2286ee23.chunk.css"
+  },
+  {
+    "revision": "b869abb01e5d1b31fe1c",
+    "url": "/covid-19/static/js/2.052d7cdf.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid-19/static/js/2.052d7cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e00445f6d038130d8e2",
+    "url": "/covid-19/static/js/main.b11cc303.chunk.js"
+  },
+  {
+    "revision": "367832239b5cbd60398d",
+    "url": "/covid-19/static/js/runtime-main.c0184dbc.js"
+  },
+  {
+    "revision": "25fbfbf108fa92df0b82131947912167",
+    "url": "/covid-19/static/media/bacteria.25fbfbf1.svg"
+  }
+]);
